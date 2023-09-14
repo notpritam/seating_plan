c58060c5fdf50b0a0cb2779940aee4d0339b99a9
notpritam <notpritamsharma@gmail.com>
2023-09-15 04:42:02 +0530

    Done

--- app/page.js
+++ app/page.js
@@ -1,113 +1,258 @@
-import Image from 'next/image'
+"use client";
+import { useEffect, useState } from "react";
+import path from "path";
 
-export default function Home() {
+const Home = () => {
+  const [gridData, setGridData] = useState([]);
+
+  useEffect(() => {
+    const file1Content = [...batch1];
+    const file2Content = [...batch2];
+    const file3Content = [...batch3];
+
+    // Get the current date
+    const currentDate = new Date();
+    const dayOfMonth = currentDate.getDate();
+
+    // Define custom row and column lengths based on the current date
+    let customRowCount1,
+      customColumnCount1,
+      customRowCount2,
+      customColumnCount2,
+      customRowCount3,
+      customColumnCount3;
+
+    if (dayOfMonth >= 1 && dayOfMonth <= 10) {
+      customRowCount1 = 9;
+      customColumnCount1 = 6;
+      customRowCount2 = 8;
+      customColumnCount2 = 6;
+      customRowCount3 = 9;
+      customColumnCount3 = 7;
+    } else if (dayOfMonth >= 11 && dayOfMonth <= 20) {
+      customRowCount1 = 8;
+      customColumnCount1 = 6;
+      customRowCount2 = 9;
+      customColumnCount2 = 7;
+      customRowCount3 = 9;
+      customColumnCount3 = 6;
+    } else if (dayOfMonth >= 21 && dayOfMonth <= 31) {
+      customRowCount1 = 9;
+      customColumnCount1 = 7;
+      customRowCount2 = 9;
+      customColumnCount2 = 6;
+      customRowCount3 = 8;
+      customColumnCount3 = 6;
+    }
+
+    // Function to create a grid with the specified dimensions
+    function createGrid(content, rowCount, columnCount) {
+      const grid = [];
+      for (let i = 0; i < rowCount; i++) {
+        const row = [];
+        for (let j = 0; j < columnCount; j++) {
+          // Check if there is data left to add
+          if (content.length > 0) {
+            const randomIndex = Math.floor(Math.random() * content.length);
+            const name = content.splice(randomIndex, 1)[0];
+            row.push(name);
+          } else {
+            row.push("");
+          }
+        }
+        grid.push(row);
+      }
+      return grid;
+    }
+
+    const grid1 = createGrid(file1Content, customRowCount1, customColumnCount1);
+    const grid2 = createGrid(file2Content, customRowCount2, customColumnCount2);
+    const grid3 = createGrid(file3Content, customRowCount3, customColumnCount3);
+
+    setGridData([grid1, grid2, grid3]);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="flex gap-8 bg-red-400 flex-nowrap">
+      {gridData.map((item) => (
+        <>
+          <div className="flex flex-nowrap gap-4">
+            {item.map((itemRow) => (
+              <>
+                <div className="">
+                  {itemRow.map((name) => (
+                    <>
+                      <div className="w-[200px]">{name}</div>
+                    </>
+                  ))}
+                </div>
+              </>
+            ))}
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default Home;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const batch1 = [
+  "Aatmik Panse",
+  "ABHINAV KUMAR JHA",
+  "Abhinav Nagar",
+  "Adarsh Singh Tomar",
+  "Amlan Buragohain",
+  "Amritesh Indal",
+  "Amrutha c a",
+  "Ansuman kumar",
+  "Arjun Kshirsagar",
+  "Aryan Choudhary",
+  "BHAVYA JAIN",
+  "Charanjeet",
+  "DHANUSH PAGOTI",
+  "Harsh Kumar",
+  "JEEVAN H",
+  "Jenish Thapa",
+  "KARTIK  JAIN",
+  "Kohantika",
+  "Kushi Varadaraj",
+  "Lokendra",
+  "Mahak Juriani",
+  "Manish Rao Rachakonda",
+  "Medha Shree",
+  "Navneet",
+  "Nishant Raj Tiwari",
+  "Parth Arora",
+  "Pooja Talele",
+  "Pranav Aggarwal",
+  "prathmesh bhardwaj",
+  "Prem B Shah",
+  "Pritam Raj",
+  "Rajat Tyagi",
+  "Rudra Chauhan",
+  "Sachin Beniwal",
+  "Saksham jain",
+  "Samrudh V V",
+  "Sandip Dey",
+  "Satish Pandit Rathod",
+  "Shivam Tiwari",
+  "SHRIMAY SOMANI",
+  "Shrinibas Mahanta",
+  "Sourashis Sarkar",
+  "Sudharsan S",
+  "Suraj",
+  "Suryansh Shakya",
+  "Syed Ayaan Ali",
+  "V Shravanth",
+  "Vinayaka Madhava",
+  "Vishesh",
+  "Zaid",
+];
+// Output the array to the console
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+const batch2 = [
+  "AAYUSH ASHOK KHOPADE",
+  "Abhinav Gupta",
+  "ABHISHEK KUMAR SHAH",
+  "Aditya Jaiswal",
+  "Aditya Prasad Dash",
+  "Aditya singh",
+  "Aman Yadav",
+  "Aniruddha Patil",
+  "Anuj",
+  "Arnavya Chettri",
+  "Arth Thapa",
+  "Atharv sanjay jain",
+  "Ayaan Khan",
+  "Ayush Kesharwani",
+  "Divyanshu Pandey",
+  "Garvit jain",
+  "GSivaRam",
+  "Harsh Kumar",
+  "Harshini Reddy Nellore",
+  "KARAN DAS A",
+  "Krupakar Reddy",
+  "Kvijay gaurav",
+  "loukik",
+  "MAYANK GUPTA",
+  "Naresh Biradar",
+  "Navneet",
+  "Nishant kumar Dwivedi",
+  "Rudraksh Rankawat",
+  "Sai Deepak",
+  "Sai Venkatesh Alampally",
+  "Sanjana",
+  "Shreshtha Sharma",
+  "Shruti Verma",
+  "Siddharth R",
+  "Souvik Kumar",
+  "Spoorthy",
+  "srikrrishn choudhary",
+  "suraj kumar",
+  "SWARNIM",
+  "Tejas Ghatule",
+  "Thirunagari Rohith",
+  "THRISHAL",
+  "Tushar Khurana",
+  "UDIT KUMAR NAYAK",
+  "Varun Deep Saini",
+  "Vatsal Omar",
+  "Vibhu",
+  "Vinay Reddy Yedula",
+  "Vivek Yadav",
+  "Yash Agarwal",
+];
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+const batch3 = [
+  "Abhinav",
+  "Aditya Kumar Singh",
+  "Ajai Sharan",
+  "Aman Anand",
+  "Archisman Midya",
+  "Arjun",
+  "Bhargav M",
+  "Bishal Jena",
+  "Chikoti Ruthwik",
+  "Daksh jain",
+  "Daksh Kanaujia",
+  "Dipti Hatwar",
+  "HARSH KUMAR PANCHAL",
+  "Harsh Kumar Prabhakar",
+  "Harsh umesh kumar",
+  "Harshit Goel",
+  "harshit sachan",
+  "Harshit Shah",
+  "Hemkesh V Kantawala",
+  "Himanshu Sainani",
+  "Ishan Avasthi",
+  "Jiya Singhal",
+  "K Manoj krishna",
+  "kartik deshpande",
+  "Keshav Makkad",
+  "Krishna Patidar",
+  "Kumar Kartikay",
+  "Naga",
+  "narasimha",
+  "Naresh Kumar Thodupunoori",
+  "Nayanshi singh",
+  "Neel Dholiya",
+  "Nymish Kashivishwanath",
+  "OM Annasaheb waghchavare",
+  "Prashant Singh Chauhan",
+  "Prince kumar",
+  "Priyanshu Baul Majumder",
+  "Rohan Danny Skaria",
+  "Samrat",
+  "Shivam Goel",
+  "Shivansh Singh",
+  "Shreyas Garg",
+  "Siddham Jain",
+  "Srujan",
+  "Sushant Singh Chauhan",
+  "Tamanna Mohan",
+  "tanvi",
+  "Utkersh Basnet",
+  "Vikramaditya",
+  "Yashraj Sunil Kupekar",
+];
