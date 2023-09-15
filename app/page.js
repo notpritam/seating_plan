c0870ba02077c2f432e05f5321b371411e84798e
notpritam <notpritamsharma@gmail.com>
2023-09-15 23:41:17 +0530

    Seating

--- app/page.js
+++ app/page.js
@@ -6,85 +6,186 @@ const Home = () => {
   const [gridData, setGridData] = useState([]);
 
   useEffect(() => {
-    const file1Content = [...batch1];
-    const file2Content = [...batch2];
-    const file3Content = [...batch3];
+    // const file1Content = [...batch1];
+    // const file2Content = [...batch2];
+    // const file3Content = [...batch3];
+
+    // // Define custom row and column lengths based on the current date
+    // let customRowCount1,
+    //   customColumnCount1,
+    //   customRowCount2,
+    //   customColumnCount2,
+    //   customRowCount3,
+    //   customColumnCount3;
+
+    // if (dayOfMonth >= 1 && dayOfMonth <= 10) {
+    //   customRowCount1 = 9;
+    //   customColumnCount1 = 6;
+    //   customRowCount2 = 8;
+    //   customColumnCount2 = 6;
+    //   customRowCount3 = 9;
+    //   customColumnCount3 = 7;
+    // } else if (dayOfMonth >= 11 && dayOfMonth <= 20) {
+    //   customRowCount1 = 8;
+    //   customColumnCount1 = 6;
+    //   customRowCount2 = 9;
+    //   customColumnCount2 = 7;
+    //   customRowCount3 = 9;
+    //   customColumnCount3 = 6;
+    // } else if (dayOfMonth >= 21 && dayOfMonth <= 31) {
+    //   customRowCount1 = 9;
+    //   customColumnCount1 = 7;
+    //   customRowCount2 = 9;
+    //   customColumnCount2 = 6;
+    //   customRowCount3 = 8;
+    //   customColumnCount3 = 6;
+    // }
+
+    // // Function to create a grid with the specified dimensions
+    // function createGrid(content, rowCount, columnCount) {
+    //   const grid = [];
+    //   for (let i = 0; i < rowCount; i++) {
+    //     const row = [];
+    //     for (let j = 0; j < columnCount; j++) {
+    //       // Check if there is data left to add
+    //       if (content.length > 0) {
+    //         const randomIndex = Math.floor(Math.random() * content.length);
+    //         const name = content.splice(randomIndex, 1)[0];
+    //         row.push(name);
+    //       } else {
+    //         row.push("");
+    //       }
+    //     }
+    //     grid.push(row);
+    //   }
+    //   return grid;
+    // }
+
+    // const grid1 = createGrid(file1Content, customRowCount1, customColumnCount1);
+    // const grid2 = createGrid(file2Content, customRowCount2, customColumnCount2);
+    // const grid3 = createGrid(file3Content, customRowCount3, customColumnCount3);
+
+    // setGridData([grid1, grid2, grid3]);
+
+    // Define custom row and column lengths based on the current date
 
     // Get the current date
     const currentDate = new Date();
     const dayOfMonth = currentDate.getDate();
-
-    // Define custom row and column lengths based on the current date
-    let customRowCount1,
-      customColumnCount1,
-      customRowCount2,
-      customColumnCount2,
-      customRowCount3,
-      customColumnCount3;
+    let batch1, batch2, batch3;
 
     if (dayOfMonth >= 1 && dayOfMonth <= 10) {
-      customRowCount1 = 9;
-      customColumnCount1 = 6;
-      customRowCount2 = 8;
-      customColumnCount2 = 6;
-      customRowCount3 = 9;
-      customColumnCount3 = 7;
+      batch1 = ["B", "C"];
+      batch2 = ["D", "E"];
+      batch3 = ["F", "G"];
     } else if (dayOfMonth >= 11 && dayOfMonth <= 20) {
-      customRowCount1 = 8;
-      customColumnCount1 = 6;
-      customRowCount2 = 9;
-      customColumnCount2 = 7;
-      customRowCount3 = 9;
-      customColumnCount3 = 6;
+      batch3 = ["B", "C"];
+      batch1 = ["D", "E"];
+      batch2 = ["F", "G"];
     } else if (dayOfMonth >= 21 && dayOfMonth <= 31) {
-      customRowCount1 = 9;
-      customColumnCount1 = 7;
-      customRowCount2 = 9;
-      customColumnCount2 = 6;
-      customRowCount3 = 8;
-      customColumnCount3 = 6;
+      batch2 = ["B", "C"];
+      batch3 = ["D", "E"];
+      batch1 = ["F", "G"];
     }
 
-    // Function to create a grid with the specified dimensions
-    function createGrid(content, rowCount, columnCount) {
-      const grid = [];
-      for (let i = 0; i < rowCount; i++) {
-        const row = [];
-        for (let j = 0; j < columnCount; j++) {
-          // Check if there is data left to add
-          if (content.length > 0) {
-            const randomIndex = Math.floor(Math.random() * content.length);
-            const name = content.splice(randomIndex, 1)[0];
-            row.push(name);
-          } else {
-            row.push("");
-          }
+    const mainGrid = [];
+
+    for (let i = 0; i < 9; i++) {
+      const row = [];
+
+      for (let j = 1; j <= 23; j++) {
+        var col = "A";
+        var available = true;
+
+        if (j > 0 && j <= 4) {
+          col = "A";
+          available = false;
+        } else if (j > 4 && j <= 7) {
+          col = "B";
+          available = true;
+        } else if (j > 7 && j <= 10) {
+          col = "C";
+          available = true;
+        } else if (j > 10 && j <= 13) {
+          col = "D";
+          available = true;
+        } else if (j > 13 && j <= 16) {
+          col = "E";
+          available = true;
+        } else if (j > 16 && j <= 20) {
+          col = "F";
+          available = true;
+        } else if (j > 20) {
+          col = "G";
+          available = true;
         }
-        grid.push(row);
+
+        row.push({
+          id: j + i,
+          name: "",
+          batch: 1,
+          availablilty: available,
+          column: col,
+        });
       }
-      return grid;
+      mainGrid.push(row);
     }
 
-    const grid1 = createGrid(file1Content, customRowCount1, customColumnCount1);
-    const grid2 = createGrid(file2Content, customRowCount2, customColumnCount2);
-    const grid3 = createGrid(file3Content, customRowCount3, customColumnCount3);
+    const finalData = [];
 
-    setGridData([grid1, grid2, grid3]);
+    for (let i = 0; i < mainGrid.length; i++) {
+      const row = [];
+
+      finalData.push(row);
+
+      for (let j = 0; j < mainGrid[i].length; j++) {
+        let currentCol = mainGrid[i][j].column;
+        let batch = 0;
+
+        if (batch1.includes(currentCol)) {
+          batch = 1;
+        } else if (batch2.includes(currentCol)) {
+          batch = 2;
+        } else if (batch3.includes(currentCol)) {
+          batch = 3;
+        }
+      }
+    }
+
+    setGridData(mainGrid);
   }, []);
 
+  function getRandomAndRemove(arr) {
+    if (arr.length === 0) {
+      return null; 
+    }
+
+    // Generate a random index
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // Extract the random element
+    const randomElement = arr[randomIndex];
+
+    // Remove the element from the array
+    arr.splice(randomIndex, 1);
+
+    return randomElement;
+  }
+
   return (
-    <div className="flex gap-8 bg-red-400 flex-nowrap">
+    <div className="flex flex-col gap-8  flex-nowrap">
       {gridData.map((item) => (
         <>
-          <div className="flex flex-nowrap gap-4">
-            {item.map((itemRow) => (
+          <div className="flex flex-nowrap  gap-4">
+            {item.map((cell) => (
               <>
-                <div className="">
-                  {itemRow.map((name) => (
-                    <>
-                      <div className="w-[200px]">{name}</div>
-                    </>
-                  ))}
+                <div className="flex w-full shadow-lg p-4 rounded-xl border flex-col gap-2">
+                  <span>name :- {cell.name}</span>
+                  <span>batch :- {cell.batch}</span>
+                  <span>column :- {cell.column}</span>
+                  <span>
+                    available :- {cell.availablilty ? "True" : "False"}
+                  </span>
                 </div>
               </>
             ))}
